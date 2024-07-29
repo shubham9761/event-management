bc8227c82ce4ac360eed41f2714ad1d9c8ced2aa
Shubham Mishra <81912992+shubham9761@users.noreply.github.com>
2024-07-29 22:44:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LMS</title>
+	<meta charset="utf-8" name="viewport" content="width=device-width,intial-scale=1">
+	<link rel="stylesheet" href="index_style.css">
+</head>
+<body>
+	<div class="main">
+		<div class="submain">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container-fluid">
+			<div class="sec">
+				<div class="welcome abox">LIBRARY MANAGEMENT SYSTEM</div>
+			</div>
+			<div class="maintain">
+				<div class="muser aboxs"><a href="vendor/vendor_login.php">Vendor Login</a></div>
+				<div class="mvendor aboxs"><a href="admin/admin_login.php">Admin Login</a></div>
+				<div class="muser aboxs"><a href="user/user_login.php">User Login</a></div>
+			</div>
+		</div>
+	</nav><br>
+		</div>
+	</div>
+</body>
+</html>
